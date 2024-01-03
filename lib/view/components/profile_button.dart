5a73592649ce212f70ef371b52784ea606628289
Lincianyong <110042648+Lincianyong@users.noreply.github.com>
2024-01-03 23:54:21 +0700

    profile page

---
+++ lib/view/components/profile_button.dart
@@ -0,0 +1,50 @@
+import 'package:dotted_line/dotted_line.dart';
+import 'package:flutter/material.dart';
+import 'package:gsc2024/model/constants.dart';
+
+class ProfileButton extends StatelessWidget {
+  final String text;
+  final Icon icon;
+
+  const ProfileButton({Key? key, required this.text, required this.icon})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        DottedLine(
+          direction: Axis.horizontal,
+          lineThickness: 1.0,
+          dashGapLength: 4.0,
+          lineLength: double.infinity,
+          dashColor: Colors.grey,
+        ),
+        Container(
+          height: 52,
+          child: Padding(
+            padding: const EdgeInsets.only(
+                left: 16.0), // Adjust the left padding as needed
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: icon,
+                ),
+                Text(
+                  text,
+                  style: TextStyle(
+                    color: AppColor.kTextColor, // Make sure AppColor is defined
+                    fontSize: 16,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/view/profilepage.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gsc2024/model/constants.dart';
+import 'package:gsc2024/view/testpage.dart';
+import 'package:gsc2024/view/components/profile_button.dart';
+import 'components/afterconnect.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:dotted_line/dotted_line.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(alignment: 
+        Alignment.center,
+        child: Container(
+          padding: EdgeInsets.only(top: 60, left: 26, right: 26, bottom: 26),
+          child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              //heade & return button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Icon(
+                        Icons.arrow_back,
+                        color: AppColor.kTextColor,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 8,
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        'Profil',
+                        style: TextStyle(
+                          color: AppColor.kTextColor,
+                          fontSize: 20,
+                          fontFamily: 'Poppins',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: SizedBox(),
+                  ),
+                ],
+              ),
+
+              //Profile
+              Column(
+                children:[
+                //image
+                Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 36),
+                    child: Container(
+                      width: 72,
+                      height: 72, 
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey,
+                        border: Border.all(
+                          color:
+                              Colors.black,
+                          width: 1.6,
+                        ),
+                      ),
+                    )
+                  ),
+                ]
+                ),
+                //name & address
+                Column(
+                  children: [
+                    //name
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 36),
+                          child: Text(
+                            'Aryiana',
+                            style: TextStyle(
+                              color: AppColor.kTextColor,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 32,
+                              fontFamily: 'Poppins',
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: 4, bottom: 56),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.location_on_sharp,
+                                color: AppColor.kTextColor,
+                              ),
+                              SizedBox(width: 8), // Adjust the width as needed
+                              Text(
+                                'Gn. Kidul, Jawa Tengah',
+                                style: TextStyle(
+                                  color: AppColor.kTextColor,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 16,
+                                  fontFamily: 'Poppins',
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    )
+
+                  ],
+                )
+              ]
+              ),
+
+              //Button
+              Column(
+              children: [
+                ProfileButton(
+                  icon: Icon(Icons.person_rounded),
+                  text: "Personal Data",
+                ),
+                ProfileButton(
+                  icon: Icon(Icons.person_rounded),
+                  text: "Device Information",
+                ),
+                ProfileButton(
+                  icon: Icon(Icons.person_rounded),
+                  text: "Contance Center",
+                ),
+                  DottedLine(
+                    direction: Axis.horizontal,
+                    lineThickness: 1.0,
+                    dashGapLength: 4.0,
+                    lineLength: double.infinity,
+                    dashColor: Colors.grey,
+                  ),
+              ],
+            ),
+            ]
+          ),
+        ),
+      ),    
+    );
+  }
+}
