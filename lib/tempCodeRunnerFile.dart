4bc1b258fc8da4ee52ce0afd1a54ef3debce91db
MaulanaArya30 <maulanaaryaw@gmail.com>
2024-02-11 22:22:53 +0700

    temprunner?

--- lib/tempCodeRunnerFile.dart
+++ lib/tempCodeRunnerFile.dart
@@ -1,10 +1,10 @@
-Widget build(BuildContext context) {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MINUM',
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
-    );
-  }
-}
+// Widget build(BuildContext context) {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'MINUM',
+//       debugShowCheckedModeBanner: false,
+//       home: LoginPage(),
+//     );
+//   }
+// }
